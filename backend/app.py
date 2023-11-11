248941fe7f6559bbe0fad9f5b17a16571c2a3f38
Sachin <144852966+SuperSachinS@users.noreply.github.com>
2023-11-11 14:23:25 -0700

    theoretical implementation of search route

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 # app.py
 
-#AIzaSyBHOjpmNJXrZHTRijtphPn6KfX3_lQACLI - google places API key
+place_api =  'AIzaSyBHOjpmNJXrZHTRijtphPn6KfX3_lQACLI'
 
 from flask import Flask, jsonify
 
@@ -14,6 +14,33 @@ def hello():
 @app.route('/about')
 def about():
     return jsonify(message="This is the about page.")    
+
+@app.route('/search')
+def search(keyword = 'farmers', location = '51.0447, -114,0719', search_radius = 20000):
+
+    if (type(keyword) == str and type(location) == str and type(search_radius) == int):
+        google_places_url = f'https://maps.googleapis.com/maps/api/place/nearbysearch/json'
+        search_parameters = {
+            'location' : location,
+            'radius' : search_radius,
+            'keyword' : keyword,
+            'key' : place_api
+        }
+        #response = requests.get(google_places_url, params = search_params) get brandon to help me
+        response = None
+    else:
+        error_message = {"error" : "invalid data type"}
+        return jsonify(error_message), 1
+    
+    if response.status_code == 200:
+        #data = response.json()
+        #results = data.get('results', [])
+        return jsonify(message='successful search')
+    else:
+        return jsonify(error='Error fetching data from Google Places API'), 2
+    
+    return jsonify(message='ggg')
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
