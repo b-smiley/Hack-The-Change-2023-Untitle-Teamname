6ee082a8c5e8560a602743a7437150595d2ac53b
Sachin <144852966+SuperSachinS@users.noreply.github.com>
2023-11-11 13:36:02 -0700

    testing syntax/functions

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,19 @@
 # app.py
+
+#AIzaSyBHOjpmNJXrZHTRijtphPn6KfX3_lQACLI - google places API key
+
 from flask import Flask, jsonify
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return jsonify(message="Hello from Flask!")
+    return jsonify(message="Hello from Sachin!")
+
+@app.route('/about')
+def about():
+    return jsonify(message="This is the about page.")    
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
